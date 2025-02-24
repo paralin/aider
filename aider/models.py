6334bd48746888cba0ff15517bfad7ce14f6b115
Christian Stewart <christian@aperture.us>
2025-02-24 12:18:20 -0800

    3.7 sonnet as default
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- aider/models.py
+++ aider/models.py
@@ -69,6 +69,7 @@ claude-3-opus-20240229
 claude-3-sonnet-20240229
 claude-3-5-sonnet-20240620
 claude-3-5-sonnet-20241022
+claude-3-7-sonnet-20250219
 """
 
 ANTHROPIC_MODELS = [ln.strip() for ln in ANTHROPIC_MODELS.splitlines() if ln.strip()]
